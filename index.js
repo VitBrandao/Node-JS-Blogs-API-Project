c34421bf09b3e3c9a52e0ea8482ed095f675b70c
vitor <vitorsbsilva@gmail.com>
2022-04-12 14:41:42 -0300

    FINALLY the basic structure is created (i hope)

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Ouvindo porta ${PORT}`));
