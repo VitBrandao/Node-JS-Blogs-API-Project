9861cef6adc47f01697a003f450a3574d2154eb3
vitor <vitorsbsilva@gmail.com>
2022-04-12 14:38:39 -0300

    fix: refactor and add belongsToMany to PostsCategory model

--- models/PostsCategory.js
+++ models/PostsCategory.js
@@ -1,28 +1,21 @@
-module.exports = (sequelize, DataTypes) => {
-  const PostsCategory = sequelize.define('PostsCategory', {
-    postId: DataTypes.INTEGER,
-    categoryId: DataTypes.INTEGER,
-  },
-  { 
-    timestamps: false,
-    tableName: 'PostsCategories',
-    underscored: true,
-  });
+module.exports = (sequelize, _DataTypes) => {
+  const PostsCategory = sequelize.define('PostsCategory', {},
+  { timestamps: false, tableName: 'PostsCategories', underscored: true });
+
+  PostsCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostsCategory,
+      foreignKey: 'post_id',
+      otherKey: 'category_id',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostsCategory,
+      foreignKey: 'category_id',
+      otherKey: 'post_id',
+    });
+  };
 
   return PostsCategory;
 };
-
-/* UserBook.associate = (models) => {
-     models.Book.belongsToMany(models.User, {
-      as: 'users',
-      through: UserBook,
-      foreignKey: 'book_id',
-      otherKey: 'user_id',
-    });
-    models.User.belongsToMany(models.Book, {
-      as: 'books',
-      through: UserBook,
-      foreignKey: 'user_id',
-      otherKey: 'book_id',
-    });
-  }; */ 
